d8b9c61f4730926c863889add7c7081fa9065a55
hazemr2121 <ha.ahmed8996@gmail.com>
2024-10-05 11:54:39 +0300

    Implementing cityList and fixing eslint error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,28 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayount from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const URL = "http://localhost:8000/cities";
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${URL}`);
+        const data = await res.json();
+        setCities(data);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -14,8 +35,14 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayount />}>
-          <Route index element={<p> List </p>} />
-          <Route path="cities" element={<p> List of cities </p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p> Countries </p>} />
           <Route path="form" element={<p> Form </p>} />
         </Route>
