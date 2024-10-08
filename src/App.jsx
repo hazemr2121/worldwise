ae5be207ed9a93053169cf0c4ddd8d846b74dfdb
hazemr2121 <ha.ahmed8996@gmail.com>
2024-10-08 08:38:24 +0300

    dynamic routes with url parameters and reading and setting query string

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AppLayount from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const URL = "http://localhost:8000/cities";
 
@@ -45,6 +46,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
