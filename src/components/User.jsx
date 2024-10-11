c09c2a5a227dca41ce6b395e1f4dcb2b34891252
hazemr2121 <ha.ahmed8996@gmail.com>
2024-10-11 09:57:07 +0300

    adding fake authentication and protect the routing and adding title and img

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/fakeAuthContext";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const user = FAKE_USER;
-
-  function handleClick() {}
+  const { logout, user: ourUser } = useAuth();
+  const user = ourUser;
+  const navigate = useNavigate();
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
@@ -26,7 +25,7 @@ export default User;
 /*
 CHALLENGE
 
-1) Add `AuthProvider` to `App.jsx`
+1) Add `AuthProvider` to `App.jsx`  DONE
 2) In the `Login.jsx` page, call `login()` from context
 3) Inside an effect, check whether `isAuthenticated === true`. If so, programatically navigate to `/app`
 4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js`
